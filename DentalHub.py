0d8cb26f377d1bbd0fc1d9547a4bf42886317b6d
Christian Zempoalteca <bunny_benito@Christians-MacBook-Air.local>
2024-05-20 08:19:55 -0700

    Installation of PyMongo for MongoDB c onnection

--- DentalHub.py
+++ DentalHub.py
@@ -1,10 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+# Routing using decorators
 @app.route("/")
 def hello():
     return "<h1>Home Page</h1>"
 
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
 
 
 
