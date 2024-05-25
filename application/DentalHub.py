00f565aad7ee304916e9c4ae7ccb8b6fe6b27b9c
Christian Zempoalteca <bunny_benito@Christians-MacBook-Air.local>
2024-05-25 08:55:15 -0700

    MongoDB showing datefield

--- application/DentalHub.py
+++ application/DentalHub.py
@@ -1,26 +1,67 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 from flask_pymongo import PyMongo
-from Prostheses import DentalProsthesis
+from pymongo.errors import PyMongoError
+import os
+import datetime
+from Prostheses import DentalProsthesis  # Import the form class
+from dotenv import load_dotenv
 
+load_dotenv()
 
-app = Flask(__name__) #Flask Instance
-app.config['SECRET_KEY'] = '0f5b00205ff5796fcd5e2362b9dbb227ec5c79bb'
-app.config['MONGO_URI'] = 'mongodb+srv://chriz24:Elnegro24@cluster0.xs9s6dd.mongodb.net/Dental-Prostheses?retryWrites=true&w=majority&appName=Cluster0'
+app = Flask(__name__)
+# Assignment of secret key inside of my .env
+app.secret_key = os.environ.get("SECRET_KEY")
 
-# Setup mongodb
-mongodb_client = PyMongo(app)
-db = mongodb_client.db
+# Connect to MongoDB
+mongo_uri = os.getenv("MONGODB_URI") # Assignment of my URI inside of my .env
+print(os.getenv("MONGODB_URI"))
+try:
+    mongo_client = PyMongo(app, uri=mongo_uri)
+    if mongo_client.db is None:
+        raise ValueError("MongoDB connection failed: Database is None")
+except Exception as e:
+    print(os.getenv("MONGODB_URI"))
+    print(f"Error connecting to MongoDB: {e}")
+    # You can also use flash messages to inform the user about the error
+    flash("Error connecting to MongoDB. Please try again later.", "error")
 
+# Access the database
+db = mongo_client.db
+print(db) #
 # Routing using decorators
-@app.route("/")
+@app.route("/home")
 def hello():
     return "<h1>Home Page</h1>"
 
-@app.route("/add_Prosthesis")
+@app.route("/add_Prosthesis", methods=["POST", "GET"])
 def prosthesis():
-    form = DentalProsthesis()
-    return render_template("prosthesis.html", form=form ) 
+    if request.method == "POST":
+        form = DentalProsthesis(request.form)
+
+        prosthesis_type = form.prosthesis_type.data
+        checkbox = form.checkbox.data
+        # selected_date_str = form.selected_date.data.strip()  # Remove leading and trailing whitespace
+        # selected_date = datetime.strptime(selected_date_str, "%Y-%m-%d").date()
+        selected_date = form.selected_date.data
+        # Convert selected_date to a datetime.datetime object with midnight time
+        selected_datetime = datetime.datetime.combine(selected_date, datetime.time.min)
+
+        print(selected_date)
+
+        db.Prostheses.insert_one({
+            "name": prosthesis_type,
+            "list": checkbox,
+            "selected_date": selected_datetime
+            })
+        
+        print(selected_date)
+        flash("Dental Prosthesis Added", "success")
+        return "/home"
+
+    else:
+        form = DentalProsthesis()
+    return render_template("prosthesis.html", form=form)
 
 # Conditional to set application into debug mode
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
--- application/Prostheses.py
+++ application/Prostheses.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired
 class DentalProsthesis(FlaskForm):
     prosthesis_type = SelectField('Prosthesis Type', choices=[('Crown','Crown'), ('Denture','Denture'), ('Partial','Partial'), ('Night Guard','Night Guard')], validators=[DataRequired()])
     checkbox = BooleanField('Checkbox')
-    date = DateField('Date')
+    selected_date = DateField('Selected Date', format='%Y-%m-%d')
     submit = SubmitField('Add Dental Prosthesis')
